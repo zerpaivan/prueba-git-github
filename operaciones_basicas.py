0dc36af68b5e768310daff77a413dd37b66fdcc0
zerpaivan <zerpaivan@gmail.com>
2021-12-02 22:55:34 -0400

    add division a operaciones basicas

--- operaciones_basicas.py
+++ operaciones_basicas.py
@@ -11,10 +11,18 @@ def multiplicar(a, b):
     return a * b
 
 
+def division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division por cero no permitida"
+
+
 if __name__ == "__main__":
 
     a = 5
-    b = 4
+    b = 0
     print("suma: ", sumar(a, b))
     print("resta: ", restar(a, b))
     print("multiplicacion: ", multiplicar(a, b))
+    print("divivion: ", division(a, b))
